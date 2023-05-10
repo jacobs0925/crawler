9ca1690695977cd726e286eb9188d807d60a5722
jacobs0925 <jjschwar@uci.edu>
2023-05-09 20:05:23 -0700

    .

--- utils/download.py
+++ utils/download.py
@@ -9,6 +9,7 @@ def makeRespDict(url, resp, content):
     resp_dict['url'] = url
     resp_dict['status'] = resp.status_code
     resp_dict['response'] = content
+    print(content)
     
     resp_dict['size'] = resp.headers.get('Content-Length')
     return resp_dict
